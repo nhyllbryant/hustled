2d99e1ca46047000d853d0ca615a35816161ebeb
Nhyll Bryant Molina <nhyllbryant_molina@yahoo.com>
2025-11-15 17:46:29 +0800

    Initial Commit

---
+++ src/main/java/com/example/hustled/HustledApplication.java
@@ -0,0 +1,13 @@
+package com.example.hustled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustledApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hustled/HustledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hustled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
